3e5893c8eb62a7fe5e9826686f924aff841079b9
Victor Matthews <victormmatthews@gmail.com>
2024-09-23 14:36:25 -0600

    update to angular material 17

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,31 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatGridListModule,
-    MatListModule,
     MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
   ],
   exports: [
     MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatGridListModule,
-    MatListModule,
     MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
   ]
 })
 export class MaterialModule { }
